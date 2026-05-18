b55133f4ac60cd70a8578bd0d81c8d777c4ca8a8
Yasir Faizan <yasirfaizan680@gmail.com>
2026-05-18 17:28:04 +0500

    DOne2

--- Client/src/api/axios.js
+++ Client/src/api/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
+  baseURL:
+    import.meta.env.VITE_API_URL ||
+    "https://medflow-production-9af6.up.railway.app/api",
 });
 
 api.interceptors.request.use((config) => {
